db025f8a5f469fb602473c62e51be65b9f707529
kononov.ruslan <kononov.ruslan@wb.ru>
2025-10-02 17:47:12 +0300

    feat(main) fix build error 3

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/cool-tags/',
-  plugins: [react()],
-})
+export default defineConfig({ plugins: [react()], base: '/' })
